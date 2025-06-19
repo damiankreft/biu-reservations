ae63056c77aaa9b3675bf0e6e72f23d1fa95e587
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2025-06-19 16:01:25 +0200

    Assign role to users authenticated with github provider.

--- src/auth.ts
+++ src/auth.ts
@@ -1,34 +1,34 @@
-import NextAuth, { type DefaultSession } from "next-auth"
+import NextAuth, { type DefaultSession } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
-import { DataSourceContext } from "./data/DataSource";
+import { DataSourceContext } from './data/DataSource';
 
-declare module "next-auth" {
-  /**
-   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  interface Session {
-    user: {
-      /** The user's role. */
-      role: string
-      active: boolean
-      /**
-       * By default, TypeScript merges new interface properties and overwrites existing ones.
-       * In this case, the default session user properties will be overwritten,
-       * with the new ones defined above. To keep the default session user properties,
-       * you need to add them back into the newly declared interface.
-       */
-    } & DefaultSession["user"]
-  }
+declare module 'next-auth' {
+    /**
+     * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            /** The user's role. */
+            role: string;
+            active: boolean;
+            /**
+             * By default, TypeScript merges new interface properties and overwrites existing ones.
+             * In this case, the default session user properties will be overwritten,
+             * with the new ones defined above. To keep the default session user properties,
+             * you need to add them back into the newly declared interface.
+             */
+        } & DefaultSession['user'];
+    }
 }
 
 // The `JWT` interface can be found in the `next-auth/jwt` submodule
-import { JWT } from "next-auth/jwt"
- 
-declare module "next-auth/jwt" {
-  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
-  interface JWT {
-    role: string
-  }
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+    /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+    interface JWT {
+        role: string;
+    }
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -40,6 +40,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                     name: profile.name || profile.login,
                     email: profile.email,
                     image: profile.avatar_url,
+                    role:
+                        DataSourceContext.users.find(
+                            (u) => u.name === profile.name,
+                        )?.role || 'user', // Default role for GitHub users
                 };
             },
         }),
@@ -47,22 +51,25 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     callbacks: {
         signIn({ user, account, profile }) {
             // Check if the user is in the DataSourceContext
-            const cachedUser = DataSourceContext.users.find(u => u.name === user.name);
+            const cachedUser = DataSourceContext.users.find(
+                (u) => u.name === user.name,
+            );
             if (cachedUser !== undefined && !cachedUser.isActive) {
-              return false;
+                return false;
             }
             return true;
         },
         jwt({ token, user }) {
             if (user) {
-              token.id = user.id;
-              let cachedUser = DataSourceContext.users.find(u => u.name === user.name);
-              if (cachedUser) {
-                token.role = 'admin';
-              } else {
-                token.role = 'user';
-              }
-
+                token.id = user.id;
+                let cachedUser = DataSourceContext.users.find(
+                    (u) => u.name === user.name,
+                );
+                if (cachedUser) {
+                    token.role = 'admin';
+                } else {
+                    token.role = 'user';
+                }
             }
             return token;
         },
