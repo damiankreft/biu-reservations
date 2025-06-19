738bdcd647d731f028417ec9511e5caf8f3a0056
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2025-06-19 12:35:23 +0200

    Fix formatting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -179,16 +179,16 @@ export default function Home() {
             </div>
 
             <dialog id="booking_modal" ref={bookingModal} className="modal">
-                    <div className="modal-box">
-                        <h3 className="font-bold text-lg">Hello!</h3>
-                        <p className="py-4">
-                            Press ESC key or click outside to close
-                        </p>
-                    </div>
-                    <form method="dialog" className="modal-backdrop">
-                        <button>close</button>
-                    </form>
-                </dialog>
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">
+                        Press ESC key or click outside to close
+                    </p>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
         </div>
     );
 }
