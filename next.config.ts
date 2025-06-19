dec851130c120b345f2a8e8c1161a19b9cae6fa6
Damian Kreft <25139693+damiankreft@users.noreply.github.com>
2025-06-19 15:56:19 +0200

    Add experimental authInterrupts feature, fix formatting.

--- next.config.ts
+++ next.config.ts
@@ -1,53 +1,58 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  logging: {
-    fetches: {
-      fullUrl: true,
-      hmrRefreshes: true,
+    output: 'standalone',
+    experimental: {
+        authInterrupts: true,
     },
-    incomingRequests: true
-  },
-  compiler: {
-    styledComponents: true,
-    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error", "warn"] } : false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/photo-**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/photo-**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-        port: "",
-        pathname: "/photo-**",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.daisyui.com",
-        port: "",
-        pathname: "/**",
-      }
-    ],
-  }
+    logging: {
+        fetches: {
+            fullUrl: true,
+            hmrRefreshes: true,
+        },
+        incomingRequests: true,
+    },
+    compiler: {
+        styledComponents: true,
+        removeConsole:
+            process.env.NODE_ENV === 'production'
+                ? { exclude: ['error', 'warn'] }
+                : false,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/photo-**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/photo-**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.pixabay.com',
+                port: '',
+                pathname: '/photo-**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.daisyui.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
